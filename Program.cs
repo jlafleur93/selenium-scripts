121870f33703c6194bf01d399ec6eecf8807a833
jlafleur93 <50718377+jlafleur93@users.noreply.github.com>
2022-04-15 02:47:07 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+class HelloSelenium {
+    static void Main() {
+        new DriverManager().SetUpDriver(new ChromeConfig());
+        var driver = new ChromeDriver();
+        try
+      {
+        // Navigate to Url
+        driver.Navigate().GoToUrl("https://google.com");
+        // Store 'google search' button web element
+        var searchBox = driver.FindElement(By.Name("q"));
+        var searchButton = driver.FindElement(By.Name("btnK"));
+
+        searchBox.SendKeys("Selenium");
+        driver.FindElement(By.Name("q")).GetAttribute("value"); // => "Selenium"
+        //Give wait time after
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+        searchButton.Click();
+        Actions actionProvider = new Actions(driver);
+        // Perform click action on the element
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(2000);
+        IWebElement linkHandle = driver.FindElement(By.XPath("/html/body/div[7]/div/div[10]/div[1]/div[2]/div[2]/div/div/div[1]/div/div[1]/div/a/h3"));
+        actionProvider.Click(linkHandle).Build().Perform();
+        String text = driver.FindElement(By.XPath("/html/body/div/main/section[1]/div/div/div/div/h1")).Text;
+        Console.WriteLine(text);
+      }
+      finally
+      {
+        driver.Quit();
+      }
+    }
+}
