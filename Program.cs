7fa976a9901b653cf4f079e8a2cdc5c9dae4cc09
jlafleur93 <50718377+jlafleur93@users.noreply.github.com>
2022-04-15 02:49:47 -0600

    updated comments in program

--- Program.cs
+++ Program.cs
@@ -24,9 +24,13 @@ class HelloSelenium {
         Actions actionProvider = new Actions(driver);
         // Perform click action on the element
         driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(2000);
+        //Find Selenium Page
         IWebElement linkHandle = driver.FindElement(By.XPath("/html/body/div[7]/div/div[10]/div[1]/div[2]/div[2]/div/div/div[1]/div/div[1]/div/a/h3"));
+        //Goto Selenium Webpage
         actionProvider.Click(linkHandle).Build().Perform();
+        //Grab Banner & Convert Into Text
         String text = driver.FindElement(By.XPath("/html/body/div/main/section[1]/div/div/div/div/h1")).Text;
+        //Write The Text to Console
         Console.WriteLine(text);
       }
       finally
